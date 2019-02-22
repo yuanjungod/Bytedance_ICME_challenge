ba07bd8fd9a61052d4b0e9c41d0afe8250f0498e
yuanjun <yuanjun@papayamobile.com>
2019-02-22 21:03:51 +0800

    asd

--- fm_train.py
+++ fm_train.py
@@ -33,15 +33,16 @@ criterion = F.binary_cross_entropy_with_logits
 
 count = 0
 load_data_time = time.time()
-for result in os.listdir("/home/yuanjun/code/Bytedance_ICME_challenge/track2/jsons"):
-    fp = open(os.path.join("/home/yuanjun/code/Bytedance_ICME_challenge/track2/jsons", result), "r")
-    result = json.load(fp)
-    fp.close()
-    # print("%s data load finished" % count, time.time() - load_data_time)
-    # print(result["index"][0])
-    deep_fm.fit2(model, optimizer, criterion, result["index"], result["value"], result["video"], result["title"],
-                 result["title_value"], result["like"], result["finish"], count,
-                 save_path="/Users/quantum/code/Bytedance_ICME_challenge/ibyte.model")
-    count += 1
-    load_data_time = time.time()
+for _ in range(5):
+    for result in os.listdir("/home/yuanjun/code/Bytedance_ICME_challenge/track2/jsons"):
+        fp = open(os.path.join("/home/yuanjun/code/Bytedance_ICME_challenge/track2/jsons", result), "r")
+        result = json.load(fp)
+        fp.close()
+        # print("%s data load finished" % count, time.time() - load_data_time)
+        # print(result["index"][0])
+        deep_fm.fit2(model, optimizer, criterion, result["index"], result["value"], result["video"], result["title"],
+                     result["title_value"], result["like"], result["finish"], count,
+                     save_path="/Users/quantum/code/Bytedance_ICME_challenge/ibyte.model")
+        count += 1
+        load_data_time = time.time()
 
