aa88b0b04d172ea6e244e298900b56f5cbc326d2
yuanjun <yuanjun@papayamobile.com>
2019-02-23 14:27:26 +0800

    asd

--- fm_submit_and_test.py
+++ fm_submit_and_test.py
@@ -4,7 +4,7 @@ import torch
 task = "like"
 deep_fm = DeepFM(9, 140000, [80000, 400, 900000, 500, 10, 90000, 80000, 30, 20], 128, task)
 
-model_path = 'params.pkl'
+model_path = '/home/yuanjun/code/Bytedance_ICME_challenge/track2/models/finish/20190223/byte_25000.model'
 deep_fm.load_state_dict(torch.load(model_path))
 deep_fm.cuda(0)
 
