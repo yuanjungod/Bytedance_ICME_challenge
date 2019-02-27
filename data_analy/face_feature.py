defc1f9da637a069a0e93c29e2be3b4490b58435
yuanjun <yuanjun@papayamobile.com>
2019-02-27 21:25:42 +0800

    bert model

--- data_analy/face_feature.py
+++ data_analy/face_feature.py
@@ -6,9 +6,11 @@ class FaceFeature(object):
     def __init__(self, face_feature_path):
         face_feature_file = open(face_feature_path)
         self.face_feature_dict = dict()
-        for line in face_feature_file.readlines():
+        line = face_feature_file.readline()
+        while line:
             face = json.loads(line)
             self.face_feature_dict[face["item_id"]] = face["face_attrs"]
+            line = face_feature_file.readline()
         face_feature_file.close()
 
     def get_face_feature(self, item_id):
