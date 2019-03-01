dffbcd248717bc81164f89e04610eb13ae87b608
yuanjun <yuanjun@papayamobile.com>
2019-03-01 20:06:38 +0800

    eval

--- model_zoo/din_xdeep_bert.py
+++ model_zoo/din_xdeep_bert.py
@@ -592,7 +592,7 @@ class DeepFM(torch.nn.Module):
             # print(loss)
             total_loss += loss.data
             if self.verbose:
-                if i % 100 == 99:  # print every 100 mini-batches
+                if self.total_count % 100 == 99:  # print every 100 mini-batches
                     eval = self.evaluate(batch_xi, batch_xv, batch_video_feature, batch_audio_feature, batch_title_feature, batch_title_value, batch_y_like_train, batch_y_finish_train)
                     print('[%d, %5d] loss: %.6f metric: like-%.6f, finish-%.6f, learn rate: %s, time: %.1f s' %
                           (count + 1, i + 1, total_loss / 100.0, eval[0], eval[1], current_learn_rate,
@@ -634,8 +634,8 @@ class DeepFM(torch.nn.Module):
             print('valid*' * 20)
             print('val [%d] loss: %.6f metric: like-%.6f,finish-%.6f, learn rate: %s,  time: %.1f s' %
                   (count + 1, valid_loss, valid_eval[0], valid_eval[1], current_learn_rate, time() - epoch_begin_time))
-            log_json = {"count": count + 1, "loss": valid_loss, "like_auc": valid_eval[0],
-                        "finish_auc": valid_eval[1], "current_learn_rate": current_learn_rate,
+            log_json = {"count": count + 1, "loss": "%s" % valid_loss, "like_auc":  "%s" % valid_eval[0],
+                        "finish_auc": "%s" % valid_eval[1], "current_learn_rate": current_learn_rate,
                         "time": time() - epoch_begin_time}
             logger.info(json.dumps(log_json))
             print('valid*' * 20)
