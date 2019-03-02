0244429ae4780251a3b68792e74d0d9c61699e17
yuanjun <yuanjun@papayamobile.com>
2019-03-02 09:36:34 +0800

    add item id

--- model_zoo/interest_pooling.py
+++ model_zoo/interest_pooling.py
@@ -28,6 +28,7 @@ class InterestPooling1(torch.nn.Module):
         # Mask for paddings
         paddings = torch.ones(din_att.size()) * (-2 ** 32 + 1)
         if self.use_cuda:
+            print(paddings.size())
             paddings = paddings.cuda()
         # bags_Xi = bags_Xi.unsqueeze(dim=1)
         # print("bags_Xi", bags_Xi)
