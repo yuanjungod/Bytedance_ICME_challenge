2cd7b04cdaae9c04d78db7b996bce1ec6997a131
yuanjun <yuanjun@papayamobile.com>
2019-03-02 08:58:37 +0800

    add item id

--- model_zoo/din_xdeep_bert.py
+++ model_zoo/din_xdeep_bert.py
@@ -71,7 +71,8 @@ class DeepFM(torch.nn.Module):
     Attention: only support logsitcs regression
     """
 
-    def __init__(self, field_size, tile_word_size, feature_sizes, video_feature_size, audio_feature_size, embedding_size=32, is_shallow_dropout=True, dropout_shallow=[0.5, 0.5],
+    def __init__(self, field_size, tile_word_size, feature_sizes, video_feature_size, audio_feature_size,
+                 embedding_size=32, is_shallow_dropout=True, dropout_shallow=[0.5, 0.5],
                  h_depth=2, deep_layers=[32, 32], is_deep_dropout=True, dropout_deep=[0.5, 0.5, 0.5],
                  deep_layers_activation='relu', n_epochs=64, batch_size=256, learning_rate=0.003,
                  optimizer_type='adam', is_batch_norm=False, verbose=True, random_seed=950104, weight_decay=0.0,
