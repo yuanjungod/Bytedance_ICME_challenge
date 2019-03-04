de7aad538ed80efdc2fdf62853b9abf74e4c331e
yuanjun <yuanjun@papayamobile.com>
2019-03-04 20:44:29 +0800

    f

--- model_zoo/din_xdeep_bert.py
+++ model_zoo/din_xdeep_bert.py
@@ -396,8 +396,9 @@ class DeepFM(torch.nn.Module):
 
             if audio_feature is not None:
                 print(type(audio_feature))
-                print(audio_feature.size())
                 print(audio_feature)
+                print(audio_feature.size())
+
                 audio_feature = self.audio_line(audio_feature)
                 audio_feature = activation(audio_feature)
                 deep_emb = torch.cat([deep_emb, audio_feature], 1)
